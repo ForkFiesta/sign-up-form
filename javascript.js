27f3ccaac7941f851d3c58906ff9c2460fcea945
Raul Resendez <raul.resendez@proton.me>
2024-10-03 11:02:18 -0500

    complete exercise per instructions

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
-document.querySelector('form').addEventListener('input', ()=>{
+
+
+
+document.querySelector('#confirm_password').addEventListener('input', ()=>{
     validateForm();
 });
 
@@ -8,17 +11,26 @@ const validateForm = () =>{
     // console.log(password.value);
     const confirmPassword = document.querySelector('#confirm_password');
     // console.log(confirmPassword.value);
+    const message = document.querySelector('#message');
 
     let isValid = true;
 
     if (password.value != confirmPassword.value)
         {
             console.log('Passwords don\'t match');
+            password.classList.add('invalid');
+            confirmPassword.classList.add('invalid');
             isValid = false;
+            message.innerHTML = "* Passwords do not match"
+            message.classList.add('error');
+
 
 
         }
         else {
             console.log('Passwords Match');
+            password.classList.remove('invalid');
+            confirmPassword.classList.remove('invalid');
+            isValid = true;
         }
 }
